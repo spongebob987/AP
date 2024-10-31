8ae1ddf3688e486143097917ee48691144d3ab0a
Louis Dumont-Derobert <louisdd27600@gmail.com>
2024-10-31 16:48:14 +0100

    cours 31/10

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,8 +1,36 @@
-<?php
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Mon portfolio</title>
+    <link href="assets/index.css" rel="stylesheet" />
+  </head>
+  <body>
+   
+  <?php
 
-require_once("data/index.yaml");
-$data = yaml_parse_file(index.yaml);
+require_once("yaml/yaml.php");
+$data = yaml_parse_file("data/index.yaml");
 
-echo "<p> Bienvenue sur le portfolio de ".$data["nom"].", ".$data["prenom"]."</p>";
+echo "<h1> Bienvenue sur le portfolio de ".$data["prenom"]." ".$data["nom"]." !"."</h1>";
+
+echo "<p> Ma phrase préférée c'est : ".$data["accroche"]."</p>";
+echo "<p>".$data["paragraphe"]."</p>";
+
+echo "<nav>";
+echo "<a href='pages/competences.php'>Mes compétences</a>";
+echo "<a href='pages/contact.php'>Contact</a>";
+echo "<a href='pages/formation.php'>Formation</a>";
+echo "<a href='pages/realisation.php'>Mes réalisations</a>";
+echo "</nav>";
+
+
+
+
+
+
 
 ?>
+
+</body>
+</html>
