8e117f640c3c69ebe010d2f63fdd1736e0fa3549
Dumont-Derobert Louis <louisdd27600@gmail.com>
2024-10-31 11:47:27 +0100

    a

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once("data/index.yaml");
-$data = yaml_parse_file(index.yaml)
+$data = yaml_parse_file(index.yaml);
 
-echo "<p> Bienvenue sur le portfolio de ".$data["prenom"]. "</p>"
+echo "<p> Bienvenue sur le portfolio de ".$data["nom"].", ".$data["prenom"]."</p>";
 
 ?>
